2c85dc52600cc9e3f207bd1ad2d65087c7355962
Luigi Poole <luigipoole@outlook.com>
2015-12-18 03:16:07 -0800

    add settings

--- src/components/Dashboard/components/settings.js
+++ src/components/Dashboard/components/settings.js
@@ -17,6 +17,27 @@ default React.createClass({
         return (
             <div className="section" style={{marginTop: '0px', height: '15px', width: '300px', right: '245px'}}>
                 <h2>Settings</h2>
+
+                <div className="settings">
+
+                    <div className="sep"/>
+                    <span>Hotspot Enabled</span>
+
+                    <div className="toggler">
+                        <input type="checkbox" id="hotspot" className="toggle" style={{display:'none'}} checked/>
+                        <label htmlFor="hotspot" className="lbl"></label>
+                    </div>
+                    <div className="sep"/>
+
+                    <p className="input" >Hotspot SSID:</p>
+                    <input className="text" />
+                    <div className="sep"/>
+
+                    <p className="input" >Hotspot Password:</p>
+                    <input className="text"  type="password" />
+                    <div className="sep"/>
+                </div>
+
             </div>
         );
     }
